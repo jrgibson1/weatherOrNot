86330a7ffaae8853b17a9c496df7957e752bd788
John Gibson <jrgibson1@me.com>
2019-01-06 20:21:20 +1100

    Initial Commit

---
+++ weatherOrNot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weatherOrNot
+//
+//  Created by John Gibson on 6/1/19.
+//  Copyright © 2019 John Gibson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
